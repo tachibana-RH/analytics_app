e15c4ea3d382bf97bb95fb217003399aaa0a92df
tachibana-RH <g1323204rh@gmail.com>
2021-01-24 17:15:46 +0900

    jupyter環境修正

--- docker/jupyterlab/init.sh
+++ docker/jupyterlab/init.sh
@@ -1,2 +1,4 @@
 cp .jalinebreak.sty ~/texmf/tex/latex/local/jalinebreak.sty
-cp .base.tplx /usr/local/lib/python3.7/dist-packages/nbconvert/templates/latex/base.tplx
+cp .base.tplx /usr/local/lib/python3.8/dist-packages/nbconvert/templates/latex/base.tplx
+
+jupyter lab --ip=0.0.0.0 --allow-root --no-browser --NotebookApp.token=''
